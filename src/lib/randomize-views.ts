45359234de566b5ce786ee11bc6b760d3302ab8b
22130107 <22130107@st.hcmuaf.edu.vn>
2026-07-02 12:16:29 +0700

    commit

--- src/lib/randomize-views.ts
+++
@@ -1,9 +0,0 @@
-import { pool } from "./db";
-
-async function run() {
-  await pool.query('UPDATE projects SET views = FLOOR(RAND() * 500) + 50');
-  console.log("Views randomized.");
-  process.exit(0);
-}
-
-run();
