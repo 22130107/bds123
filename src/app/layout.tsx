1c64df17240aa2f6776b22f234b18d3bc5905b8e
22130107 <22130107@st.hcmuaf.edu.vn>
2026-07-16 21:23:15 +0700

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,8 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&family=Playfair+Display:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
         {primaryColor && (
           <style dangerouslySetInnerHTML={{ __html: `
             :root {
